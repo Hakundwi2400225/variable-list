f3894f8232d27df69482aabcddde100750e2579f
Hakundwi <givenhakundwi7@gmail.com>
2026-08-19 13:17:35 +0200

    initial commit

---
+++ src/main/java/com/mycompany/parameter/Parameter.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parameter;
+
+/**
+ *
+ * @author hakun
+ */
+public class Parameter {
+
+    public static void main(String[] args) {
+        
+        int var1;
+        int var2;
+        int var3;
+        var1=200;
+        System.out.println("var1 = "+var1);
+        var2= var1/2;
+        System.out.println("var2 = "+var2);
+        var3= (var2*var1)/2+(var1*5)-var2*4;
+        System.out.println("var3 ="+var3);
+    }
+}
